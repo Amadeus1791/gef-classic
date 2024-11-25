6c422282fe1da7071dc4e3a15b3ad6d8e9c66fa6
asdf <asdf>
2024-11-25 20:42:34 +0100

    Modernized LED

--- org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/figures/LEDFigure.java
+++ org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/figures/LEDFigure.java
@@ -1,21 +1,11 @@
-/*******************************************************************************
- * Copyright (c) 2000, 2022 IBM Corporation and others.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0.
- *
- * SPDX-License-Identifier: EPL-2.0
- *
- * Contributors:
- *     IBM Corporation - initial API and implementation
- *******************************************************************************/
 package org.eclipse.gef.examples.logicdesigner.figures;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
 
 import org.eclipse.draw2d.ColorConstants;
+import org.eclipse.draw2d.FigureUtilities;
 import org.eclipse.draw2d.Graphics;
 import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.draw2d.geometry.Insets;
@@ -27,55 +17,46 @@ import org.eclipse.gef.handles.HandleBounds;
 
 import org.eclipse.gef.examples.logicdesigner.model.LED;
 
-/**
- * @author danlee
- */
 public class LEDFigure extends NodeFigure implements HandleBounds {
 	public static final Dimension SIZE = new Dimension(61, 47);
 
-	/**
-	 * Color of the shadow around the LEDFigure's display
-	 */
-	public static final Color DISPLAY_SHADOW = new Color(null, 57, 117, 90);
+	public static final Color DISPLAY_TEXT = new Color(null, 255, 187, 51); // Warmer amber for better readability
+	public static final Color BODY_COLOR = new Color(null, 67, 160, 120); // More vibrant teal-green
+	public static final Color CONNECTOR_COLOR = new Color(null, 57, 150, 110); // Slightly darker for connectors
 
-	/**
-	 * Color of the LEDFigure's displayed value
-	 */
-	public static final Color DISPLAY_TEXT = new Color(null, 255, 199, 16);
+	// Modern font for display
+	protected static final Font DISPLAY_FONT = new Font(null, "Consolas", 16, SWT.NORMAL);
 
-	protected static final Font DISPLAY_FONT = new Font(null, "", 19, 0); //$NON-NLS-1$
+	// Display rectangle
+	protected static Rectangle displayRectangle = new Rectangle(15, 11, 31, 25);
+
 	protected static PointList connector = new PointList();
 	protected static PointList bottomConnector = new PointList();
-	protected static Rectangle displayRectangle = new Rectangle(15, 11, 31, 25);
-	protected static Rectangle displayShadow = new Rectangle(14, 10, 32, 26);
-	protected static Rectangle displayHighlight = new Rectangle(15, 11, 32, 26);
-	protected static Point valuePoint = new Point(16, 10);
 
 	static {
+		// Smoother connector shape
 		connector.addPoint(-2, 0);
-		connector.addPoint(1, 0);
-		connector.addPoint(2, 1);
-		connector.addPoint(2, 5);
-		connector.addPoint(-1, 5);
-		connector.addPoint(-1, 1);
+		connector.addPoint(2, 0);
+		connector.addPoint(3, 1);
+		connector.addPoint(3, 4);
+		connector.addPoint(-2, 4);
+		connector.addPoint(-2, 1);
 
 		bottomConnector.addPoint(-2, 0);
-		bottomConnector.addPoint(1, 0);
-		bottomConnector.addPoint(2, -1);
-		bottomConnector.addPoint(2, -5);
-		bottomConnector.addPoint(-1, -5);
-		bottomConnector.addPoint(-1, -1);
+		bottomConnector.addPoint(2, 0);
+		bottomConnector.addPoint(3, -1);
+		bottomConnector.addPoint(3, -4);
+		bottomConnector.addPoint(-2, -4);
+		bottomConnector.addPoint(-2, -1);
 	}
 
 	protected static final int[] GAP_CENTERS_X = { 8, 23, 38, 53 };
 	protected static final int Y1 = 2;
 	protected static final int Y2 = 44;
+	private static final int CORNER_RADIUS = 6;
 
 	protected String value;
 
-	/**
-	 * Creates a new LEDFigure
-	 */
 	public LEDFigure() {
 		FixedConnectionAnchor c;
 		c = new FixedConnectionAnchor(this);
@@ -114,89 +95,78 @@ public class LEDFigure extends NodeFigure implements HandleBounds {
 		c.topDown = false;
 		connectionAnchors.put(LED.TERMINAL_4_OUT, c);
 		outputConnectionAnchors.add(c);
-
 	}
 
-	/**
-	 * @see org.eclipse.gef.handles.HandleBounds#getHandleBounds()
-	 */
+	@Override
+	protected void paintFigure(Graphics g) {
+		Rectangle r = getBounds().getCopy();
+		g.translate(r.getLocation());
+
+		// Draw main body with rounded corners using new color
+		g.setBackgroundColor(BODY_COLOR);
+		Rectangle mainBody = new Rectangle(0, 2, r.width, r.height - 4);
+		g.fillRoundRectangle(mainBody, CORNER_RADIUS, CORNER_RADIUS);
+
+		// Draw connectors with new colors
+		drawConnectors(g, r);
+
+		// Draw display with rounded corners
+		g.setBackgroundColor(ColorConstants.black);
+		g.fillRoundRectangle(displayRectangle, CORNER_RADIUS, CORNER_RADIUS);
+
+		// Draw text
+		if (value != null) {
+			drawModernText(g);
+		}
+	}
+
+	private void drawConnectors(Graphics g, Rectangle r) {
+		for (int i = 0; i < 4; i++) {
+			// Draw connector gaps
+			g.setForegroundColor(ColorConstants.listBackground);
+			g.drawLine(GAP_CENTERS_X[i] - 2, Y1, GAP_CENTERS_X[i] + 3, Y1);
+			g.drawLine(GAP_CENTERS_X[i] - 2, Y2, GAP_CENTERS_X[i] + 3, Y2);
+
+			// Use new connector color
+			g.setForegroundColor(CONNECTOR_COLOR);
+			g.setBackgroundColor(CONNECTOR_COLOR);
+
+			connector.translate(GAP_CENTERS_X[i], 0);
+			g.fillPolygon(connector);
+			connector.translate(-GAP_CENTERS_X[i], 0);
+
+			bottomConnector.translate(GAP_CENTERS_X[i], r.height - 1);
+			g.fillPolygon(bottomConnector);
+			bottomConnector.translate(-GAP_CENTERS_X[i], -(r.height - 1));
+		}
+	}
+
+	private void drawModernText(Graphics g) {
+		g.setFont(DISPLAY_FONT);
+		Dimension textSize = FigureUtilities.getTextExtents(value, DISPLAY_FONT);
+
+		// Center the text both horizontally and vertically
+		int x = displayRectangle.x + (displayRectangle.width - textSize.width) / 2;
+		int y = displayRectangle.y + (displayRectangle.height - textSize.height) / 2;
+
+		g.setForegroundColor(DISPLAY_TEXT);
+		g.drawText(value, new Point(x, y));
+	}
+
 	@Override
 	public Rectangle getHandleBounds() {
 		return getBounds().getShrinked(new Insets(2, 0, 2, 0));
 	}
 
-	/**
-	 * @see org.eclipse.draw2d.Figure#getPreferredSize(int, int)
-	 */
 	@Override
 	public Dimension getPreferredSize(int wHint, int hHint) {
 		return SIZE;
 	}
 
-	/**
-	 * @see org.eclipse.draw2d.Figure#paintFigure(Graphics)
-	 */
-	@Override
-	protected void paintFigure(Graphics g) {
-		Rectangle r = getBounds().getCopy();
-		g.translate(r.getLocation());
-		g.setBackgroundColor(LogicColorConstants.logicGreen);
-		g.setForegroundColor(LogicColorConstants.connectorGreen);
-		g.fillRectangle(0, 2, r.width, r.height - 4);
-		int right = r.width - 1;
-		g.drawLine(0, Y1, right, Y1);
-		g.drawLine(0, Y1, 0, Y2);
-
-		g.setForegroundColor(LogicColorConstants.connectorGreen);
-		g.drawLine(0, Y2, right, Y2);
-		g.drawLine(right, Y1, right, Y2);
-
-		// Draw the gaps for the connectors
-		g.setForegroundColor(ColorConstants.listBackground);
-		for (int i = 0; i < 4; i++) {
-			g.drawLine(GAP_CENTERS_X[i] - 2, Y1, GAP_CENTERS_X[i] + 3, Y1);
-			g.drawLine(GAP_CENTERS_X[i] - 2, Y2, GAP_CENTERS_X[i] + 3, Y2);
-		}
-
-		// Draw the connectors
-		g.setForegroundColor(LogicColorConstants.connectorGreen);
-		g.setBackgroundColor(LogicColorConstants.connectorGreen);
-		for (int i = 0; i < 4; i++) {
-			connector.translate(GAP_CENTERS_X[i], 0);
-			g.fillPolygon(connector);
-			g.drawPolygon(connector);
-			connector.translate(-GAP_CENTERS_X[i], 0);
-
-			bottomConnector.translate(GAP_CENTERS_X[i], r.height - 1);
-			g.fillPolygon(bottomConnector);
-			g.drawPolygon(bottomConnector);
-			bottomConnector.translate(-GAP_CENTERS_X[i], -r.height + 1);
-		}
-
-		// Draw the display
-		g.setBackgroundColor(LogicColorConstants.logicHighlight);
-		g.fillRectangle(displayHighlight);
-		g.setBackgroundColor(DISPLAY_SHADOW);
-		g.fillRectangle(displayShadow);
-		g.setBackgroundColor(ColorConstants.black);
-		g.fillRectangle(displayRectangle);
-
-		// Draw the value
-		g.setFont(DISPLAY_FONT);
-		g.setForegroundColor(DISPLAY_TEXT);
-		g.drawText(value, valuePoint);
-	}
-
-	/**
-	 * Sets the value of the LEDFigure to val.
-	 *
-	 * @param val The value to set on this LEDFigure
-	 */
 	public void setValue(int val) {
 		String newValue = String.valueOf(val);
-		if (val < 10)
-		 {
-			newValue = "0" + newValue; //$NON-NLS-1$
+		if (val < 10) {
+			newValue = "0" + newValue;
 		}
 		if (newValue.equals(value)) {
 			return;
@@ -205,13 +175,16 @@ public class LEDFigure extends NodeFigure implements HandleBounds {
 		repaint();
 	}
 
-	/**
-	 *
-	 * @see java.lang.Object#toString()
-	 */
 	@Override
 	public String toString() {
-		return "LEDFigure"; //$NON-NLS-1$
+		return "LEDFigure";
+	}
+
+	public void dispose() {
+		DISPLAY_TEXT.dispose();
+		BODY_COLOR.dispose();
+		CONNECTOR_COLOR.dispose();
+		DISPLAY_FONT.dispose();
 	}
 
 }
